626efba7f7128dc1c3adcd4d76a0151a92606b47
Hyeon Yujin <gusdbwls9772@gmail.com>
2021-01-07 23:58:14 +0900

    Correct the wrong😓

--- Code_Festival/01/41~50/50.js
+++ Code_Festival/01/41~50/50.js
@@ -3,9 +3,9 @@
 
 function bubble(arr) {
     let result = arr.slice();
-  
+
     for (let i = 0; i < result.length - 1; i++) {
-      for (let j = 0; j < i + 1; j++) {
+      for (let j = 0; j < result.length - i; j++) { // 이미 검사한 배열 요소는 검사할 필요가 없다. 따라서 - i
         if (result[j] > result[j + 1]) {
           let temp = result[j];
           result[j] = result[j + 1];
@@ -13,14 +13,13 @@ function bubble(arr) {
         }
       }
     }
-  
+
     return result;
   }
-  
+
 const items = "4 2 3 8 5".split(" ").map((a) => {
     return parseInt(a, 10);
 });
-  
+
 console.log(items);
 console.log(bubble(items));
-  
