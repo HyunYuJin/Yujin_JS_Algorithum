b183a520d86c0a68895a5038665f5cda4f622f00
현유진 <gusdbwls9772@gmail.com>
2021-01-12 17:06:27 +0900

    ☠Supplementation of incorrect content

--- Code_Festival/02/61~70/65.js
+++ Code_Festival/02/61~70/65.js
@@ -2,15 +2,49 @@
 // b = [a, b, c, d]
 // 이런 리스트가 있을 때 [[1, a], [b, 2], [3, c], [d, 4]] 이런 식으로 a, b 리스트가 번갈아가면서 출력되도록하기
 
+// const a = [1, 2, 3, 4];
+// const b = ["a", "b", "c", "d"];
+
+// let sumAB = [];
+
+// for (let i = 0; i < a.length; i++) {
+//   sumAB.push([a[i], b[i]]);
+// }
+
+// for (let i of sumAB) {
+//   console.log(i);
+// }
+
+
+// ------------------------------------------------------
+
+// 번갈아가면서 출력해야했어!!! 문제 똑바로 읽자!!
+// 짝수, 홀수 index를 사용한다.
+
 const a = [1, 2, 3, 4];
 const b = ["a", "b", "c", "d"];
+let c = [];
 
-let sumAB = [];
+// map 사용
+c = a.map((a, e) => {
+  if (e % 2 === 0) {
+    // 짝수
+    return [a, b[e]];
+  } else {
+    return [b[e], a];
+  }
+});
 
-for (let i = 0; i < a.length; i++) {
-  sumAB.push([a[i], b[i]]);
-}
+// ForEach 사용
+// a.forEach((a, e) => {
+//   if (e % 2 === 0) {
+//     // 짝수
+//     c.push([a, b[e]]);
+//   } else {
+//     c.push([b[e], a]);
+//   }
+// });
 
-for (let i of sumAB) {
-  console.log(i);
-}
+// map과 forEach는 return 값에 차이가 있고 거의 비슷하다.
+
+console.log(c);
